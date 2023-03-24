2b2c526f1488b829209c1481fbdd18dec8b1e58a
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-24 19:32:22 -0400

    Pass dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,32 @@
-
+function createDragon(name, rider, temperment) {
+  return (dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true,
+  });
+}
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten > 2) {
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+function findFireBreathers(dragons) {
+  var fireBreathers = dragons.filter(
+    (dragon) => dragon.temperment === 'aggressive'
+  );
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
-}
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers,
+};
