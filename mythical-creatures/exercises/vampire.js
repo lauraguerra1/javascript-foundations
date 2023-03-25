0e00b91ff87e25eb905f9f21f3f5a332ee488413
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-25 13:30:13 -0400

    Pass vampire tests

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,45 @@
-
+function createVampire(name, pet) {
+  let vampire = {
+    name: name,
+    pet: pet ||  'bat',
+    thirsty: true,
+    ouncesDrank: 0
+  }
+  return vampire;
+}
+function encounterDeliciousVictim(vampire) {
+ if(vampire.thirsty) {
+  return `I WANT TO SUCK YOUR BLOOD!`
+ } else {
+  return `No thanks, I am too full.`
+ }
+}
+function drink(vampire) {
+  if(vampire.ouncesDrank === 40) {
+    vampire.ouncesDrank += 10;
+    vampire.thirsty = false;
+  } else if (vampire.thirsty) {
+    vampire.ouncesDrank += 10;
+  } 
+  return vampire;
+}
+function inquirePlace(locations, location) {
+ if(locations.includes(location)) {
+  return `Yes, I have spent some time in ${location}.`;
+ } else {
+  return `No, I have never been to ${location}.`;
+ }
+}
+function findBatLovers(vampires) {
+ var batLovers = vampires.filter((vampire) => vampire.pet === 'bat')
+ var batLoverNames = batLovers.map((vampire) => vampire.name)
+ return batLoverNames;
+}
 
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+  createVampire, 
+  drink, 
+  findBatLovers, 
+  encounterDeliciousVictim, 
+  inquirePlace
 }
