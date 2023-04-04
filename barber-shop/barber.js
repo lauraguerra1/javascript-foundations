73e9661d700367cd38b544bae0b3ff2650773f36
Laura Guerra <l.garciaguerra1@gmail.com>
2023-04-03 20:43:11 -0400

    Pass barber tests

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,36 @@
+function createBarber(name, earnings, haircuts) {
+ return {
+    name: name,
+    earnings: earnings || 0,
+    haircuts: haircuts || []
+ }
+}
 
+function giveCompliment(haircut) {
+  return `This ${haircut.style} looks great!`
+}
 
-module.exports = {  };
+function cutHair(barber, haircut) {
+ barber.haircuts.push(haircut);
+ barber.earnings += haircut.price;
+ return barber;
+}
+
+function listStyles(barber, styleLength) {
+//  var haircuts = barber.haircuts.filter((haricut) => haricut.hairLength === styleLength)
+//  return haircuts.map((haircut) => haircut.style)
+  var haircuts = [];
+  for (var i = 0; i < barber.haircuts.length; i ++) {
+    if (barber.haircuts[i].hairLength === styleLength) {
+      haircuts.push(barber.haircuts[i].style);
+    }
+  }
+  return haircuts;
+}
+
+module.exports = { 
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+ };
